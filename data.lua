020a31945c1c582ab8ccdeb41fd1e6c16bc91325
Kirazy <35714562+kirazy@users.noreply.github.com>
2020-02-22 23:41:01 -0800

    Disable incomplete features for 0.18.5 release

--- data.lua
+++ data.lua
@@ -10,9 +10,6 @@ require("prototypes.recipe.loader-recipe-belt-overhaul")
 require("prototypes.recipe.loader-recipe-update")
 require("prototypes.entity.loader-particles")
 require("prototypes.entity.loader-explosions")
-require("prototypes.entity.loader-remnants")
+-- require("prototypes.entity.loader-remnants")
 require("prototypes.entity.loader-entity")
 require("prototypes.technology.loader-technology")
-
--- Debug loader
--- vanillaHD.addLoader("test-loader", vanillaHD.tint_mask["test-loader"], "transport-belt", "logistics", "loader")
