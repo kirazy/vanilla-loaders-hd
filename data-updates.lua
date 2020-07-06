5f634b39e895b2b5cfb8ce536af96b7ea46f33d8
Kirazy <35714562+kirazy@users.noreply.github.com>
2020-07-06 01:32:36 -0700

    Belt ordering for vanilla must be done in data-updates

--- data-updates.lua
+++ data-updates.lua
@@ -2,3 +2,8 @@
 -- Part of Vanilla Loaders HD
 --     
 -- See LICENSE.md in the project directory for license information.
+
+-- Set ordering in crafting window
+vanillaHD.patchLoaderItemOrder("loader","transport-belt")
+vanillaHD.patchLoaderItemOrder("fast-loader","fast-transport-belt")
+vanillaHD.patchLoaderItemOrder("express-loader","express-transport-belt")
